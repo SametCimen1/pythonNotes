367cb1372bf46626f0fd6b62923f4dbe2b5404f2
Samet Cimen <78915841+SametCimen1@users.noreply.github.com>
2022-01-27 09:33:08 -0500

    Update notes.py

--- notes.py
+++ notes.py
@@ -1,1 +1,5 @@
 # type(varible) = returns the variable type
+import tkinter
+
+tkinter.Tcl().eval('info patchlevel') -> returns the tkinter version
+def calculate(*args) -> doesnt limit the type and number of parameters.
